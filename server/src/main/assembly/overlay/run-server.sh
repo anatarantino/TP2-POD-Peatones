2da759c8e015764088065174346ded4569253d6d
martina <martinrodriguez@itba.edu.ar>
2022-10-27 20:17:47 -0300

    fixes

--- server/src/main/assembly/overlay/run-server.sh
+++ server/src/main/assembly/overlay/run-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED -cp 'lib/jars/*' "ar.edu.itba.pod.server.Server" $*
+java "$@" -cp 'lib/jars/*' "ar.edu.itba.pod.server.Server"
